08be589b13ee18a80511a78d08a3ea2a78ac41cf
Sabin Raj <sawbean.raz@googlemail.com>
2018-07-05 16:23:45 +0100

    Add: Order Model

---
+++ models/orderModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  orderItems: [
+    {
+      name: { type: String, required: true },
+      price: { type: Number, required: true },
+      options: [
+        {
+          description: { type: String, required: true },
+          addtionalCost: { type: Number, required: true }
+        }
+      ],
+      itemTotal: { type: String, required: true }
+    }
+  ],
+  subTotal: {
+    type: Number,
+    required: true
+  },
+  discount: {
+    type: Number,
+    required: true
+  },
+  orderTotal: {
+    type: Number,
+    required: true
+  },
+  orderType: {
+    type: String,
+    required: true,
+    enumValues: ["delivery", "collection"]
+  },
+  deliveryAddress: {
+    name: { type: String, required: true },
+    contactNo: { type: String, required: true },
+    address: { type: String, required: true },
+    postCode: { type: String, required: true }
+  },
+  note: { type: String }
+});
+
+module.exports = Order = mongoose.model("orders", OrderSchema);
