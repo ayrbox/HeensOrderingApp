44ac1a18e6e28a9ba047c3526ab16278732e9768
Sabin Raj <sawbean.raz@googlemail.com>
2018-07-25 19:45:34 +0100

    Menus and catgories in order taking UI

--- client/src/views/orders/take-order.js
+++ client/src/views/orders/take-order.js
@@ -1,14 +1,83 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+//components
 import MainLayout from "../viewcomponents/MainLayout";
 
+import { getMenus } from "../../actions/menuActions";
+import { getCategories } from "../../actions/categoryActions";
+
 class TakeOrder extends Component {
+  componentDidMount() {
+    if (this.props.categories.list.length === 0) {
+      this.props.getCategories();
+    }
+
+    if (this.props.menus.list.length === 0) {
+      this.props.getMenus();
+    }
+  }
   render() {
     return (
       <MainLayout>
-        <h1>Take Order</h1>
+        <div className="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+          <h1 className="display-4">Take Order</h1>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <div
+                style={{
+                  border: "1px solid #ddd",
+                  borderRadius: "8px",
+                  padding: "20px"
+                }}
+              >
+                <h2>Order</h2>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div
+                style={{
+                  border: "1px solid #ddd",
+                  borderRadius: "8px",
+                  padding: "20px"
+                }}
+              >
+                <h2>Categories</h2>
+                {this.props.categories.list.map(c => (
+                  <span
+                    className="btn btn-outline-secondary btn-lg"
+                    key={c._id}
+                  >
+                    {c.name}
+                  </span>
+                ))}
+                <h2>Menus</h2>
+                {this.props.menus.list.map(m => (
+                  <span
+                    className="btn btn-outline-secondary btn-lg"
+                    key={m._id}
+                  >
+                    {m.name}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </MainLayout>
     );
   }
 }
 
-export default TakeOrder;
+const mapStateToProps = state => ({
+  menus: state.menus,
+  categories: state.categories
+});
+
+export default connect(
+  mapStateToProps,
+  { getCategories, getMenus }
+)(TakeOrder);
