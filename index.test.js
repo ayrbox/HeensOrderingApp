c9494d9fe6859fdbf5e8d9cc06019e4c1d501a60
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-01-08 21:58:49 +0000

    Add mocha and chai for testing

---
+++ index.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+
+describe('add unit test for express', () => {
+  it('should always pass', () => {
+    const expected = 100;
+    expect(100).to.equal(expected);
+  });
+});
