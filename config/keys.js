14a9f55995836c82fccb80ecaafd114273bc596e
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-04-11 23:25:44 +0100

    ADD: config keys for test

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,11 @@
 const keysProduction = require('./keys_prod');
 const keysDevelopment = require('./keys_dev');
+const keysTest = require('./keys_test');
 
 if (process.env.NODE_ENV === 'production') {
   module.exports = keysProduction;
+} else if (process.env.NODE_ENV === 'test') {
+  module.exports = keysTest;
 } else {
   module.exports = keysDevelopment;
 }
