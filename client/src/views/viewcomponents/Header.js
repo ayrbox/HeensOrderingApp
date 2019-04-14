5739822e69731fed4f0b08107047aca94b47f6bb
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-04-14 23:44:44 +0100

    CHANGE: navbar header

--- client/src/views/viewcomponents/Header.js
+++ client/src/views/viewcomponents/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
 const Header = ({ title }) => (
-  <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+  <nav className="navbar navbar-expand-md navbar-main">
     <div className="container">
       <NavLink to="/" className="navbar-brand">
         {title}
@@ -47,20 +47,6 @@ const Header = ({ title }) => (
             </NavLink>
           </li>
         </ul>
-        <form className="form-inline mt-2 mt-md-0">
-          <input
-            className="form-control mr-sm-2"
-            type="text"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
       </div>
     </div>
   </nav>
