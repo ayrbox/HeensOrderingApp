c9b2bb8f903a0660d9301cc0d3e78cdea64d96fd
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-05-05 04:58:43 +0100

    UPDATE: menu list to use pagecontext with delete

--- client/src/views/menus/menu-list.js
+++ client/src/views/menus/menu-list.js
@@ -1,71 +1,99 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 
+import { usePageState, ACTIONS } from '../../components/PageProvider';
 import MainLayout from '../viewcomponents/MainLayout';
 import DataTable from '../../components/DataTable';
 import PageHeader from '../../components/PageHeader';
-import Fetch from '../../components/Fetch';
+import { getMenus, deleteMenu } from '../../api/menus';
 
 import styles from './styles';
 
-const MenuList = ({ classes }) => (
-  <MainLayout>
-    <div className={classes.contentWrapper}>
-      <PageHeader
-        title="Menu"
-        subTitle="List of menu"
-        addButtonLink={{
-          pathname: '/menus/add',
-          state: { modal: true },
-        }}
-      />
-      <Fetch url="/api/menus">
-        {
-          ({ loading, data: menus }) => {
-            if (loading) {
-              return <p>Loading....</p>;
-            }
+const MenuList = ({ classes }) => {
+  const [{ loading, data }, dispatch] = usePageState();
 
-            return (
-              <DataTable
-                columns={[
-                  {
-                    name: '_id',
-                    label: 'Menu Id',
-                    key: true,
-                    hidden: true,
-                  }, {
-                    name: 'name',
-                    label: 'Menu Name',
-                  }, {
-                    name: 'description',
-                    label: 'Description',
-                  }, {
-                    name: 'price',
-                    label: 'Price',
-                  }, {
-                    name: 'category',
-                    label: 'Category',
-                    render: value => <span>{value.name}</span>,
-                  }, {
-                    name: 'tags',
-                    label: 'Tags',
-                    render: tag => tag.map(t => (
-                      <label key={t}>{t}</label>
-                    )),
-                  },
-                ]}
-                data={menus}
-              />
-            );
-          }
+  const handleFetch = () => {
+    dispatch({
+      type: ACTIONS.FETCHING,
+    });
+
+    getMenus().then(({ data: menus }) => {
+      dispatch({
+        type: ACTIONS.FETCHED,
+        payload: menus,
+      });
+    });
+  };
+
+  useEffect(handleFetch, []);
+
+  return (
+    <MainLayout>
+      <div className={classes.contentWrapper}>
+        <PageHeader
+          title="Menu"
+          subTitle="List of menu"
+          addButtonLink={{
+            pathname: '/menus/add',
+            state: { modal: true },
+          }}
+          onAddClicked={() => dispatch({
+            type: ACTIONS.ADD,
+          })}
+        />
+        {
+          loading ? <p>Loading...</p> : (
+            <DataTable
+              columns={[
+                {
+                  name: '_id',
+                  label: 'Menu Id',
+                  key: true,
+                  hidden: true,
+                }, {
+                  name: 'name',
+                  label: 'Menu Name',
+                }, {
+                  name: 'description',
+                  label: 'Description',
+                }, {
+                  name: 'price',
+                  label: 'Price',
+                }, {
+                  name: 'category',
+                  label: 'Category',
+                  render: value => <span>{value.name}</span>,
+                }, {
+                  name: 'tags',
+                  label: 'Tags',
+                  render: tag => tag.map(t => (
+                    <label key={t}>{t}</label>
+                  )),
+                },
+              ]}
+              data={data}
+              onDelete={async (menuId) => {
+                try {
+                  dispatch({ type: ACTIONS.DELETING });
+                  await deleteMenu(menuId);
+                  dispatch({ type: ACTIONS.DELETED });
+                  handleFetch();
+                } catch (err) {
+                  dispatch({
+                    type: ACTIONS.ERROR,
+                    payload: err,
+                  });
+                }
+              }}
+            />
+          )
         }
-      </Fetch>
-    </div>
-  </MainLayout>
-);
+      </div>
+    </MainLayout>
+  );
+};
 
 MenuList.propTypes = {
   classes: PropTypes.shape().isRequired,
