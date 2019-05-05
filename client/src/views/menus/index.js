607e67f64814bd913ff9769d9c2128ab34c1ef63
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-05-05 04:57:58 +0100

    UPDATE: export menu without routes

--- client/src/views/menus/index.js
+++ client/src/views/menus/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
-// views
+import PageProvider from '../../components/PageProvider';
 import MenuList from './menu-list';
-import AddMenu from './add-menu';
-import EditMenu from './edit-menu';
-import DeleteMenu from './delete-menu';
-import Menu from './menu';
-import AddMenuOption from './add-menu-option';
 
 const MenuIndex = () => (
-  <div>
-    <Switch>
-      <Route path="/menus" component={MenuList} />
-    </Switch>
-    <Switch>
-      <Route exact path="/menus/add" component={AddMenu} />
-      <Route exact path="/menus/:id" component={Menu} />
-      <Route exact path="/menus/:id/edit" component={EditMenu} />
-      <Route exact path="/menus/:id/delete" component={DeleteMenu} />
-      <Route exact path="/menus/:id/options/add" component={AddMenuOption} />
-    </Switch>
-  </div>
+  <PageProvider>
+    <MenuList />
+  </PageProvider>
 );
 
 export default MenuIndex;
