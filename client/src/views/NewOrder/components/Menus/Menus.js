5b1df6db6fdf22c320d06081637079dd459cfc00
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-06-02 23:51:06 +0100

    Fix: linting

--- client/src/views/NewOrder/components/Menus/Menus.js
+++ client/src/views/NewOrder/components/Menus/Menus.js
@@ -4,7 +4,6 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 
 import GridList from '@material-ui/core/GridList';
