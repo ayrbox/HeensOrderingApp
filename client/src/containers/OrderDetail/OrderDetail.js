85d4dc5ed763bbe3e6a61082d3092d5c691daa19
Ayrbox <srdx7@hotmail.com>
2019-06-06 21:49:06 +0100

    WIP: order details

--- client/src/containers/OrderDetail/OrderDetail.js
+++ client/src/containers/OrderDetail/OrderDetail.js
@@ -1,17 +1,47 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+
 
 const OrderDetail = ({
   order,
-}) => (
-  <Fragment>
-    <Typography variant="h6">Order Details</Typography>
-    <pre style={{ fontSize: '0.5rem' }}>
-      {JSON.stringify(order, null, 2)}
-    </pre>
-  </Fragment>
-);
+}) => {
+  const {
+    orderType,
+    orderItems,
+  } = order;
+
+  return (
+    <Fragment>
+      <Typography variant="h6">Order Details</Typography>
+      <Divider />
+      <Typography varaint="h1">
+        {orderType}
+      </Typography>
+
+      <table>
+        {orderItems.map(({ description, itemTotal }) => (
+          <tr>
+            <td>
+              <Typography variant="body1">
+                {description}
+              </Typography>
+            </td>
+            <td>
+              &pound;
+              {itemTotal.toFixed(2)}
+            </td>
+          </tr>
+        ))}
+      </table>
+
+      <pre style={{ fontSize: '0.5rem' }}>
+        {JSON.stringify(order, null, 2)}
+      </pre>
+    </Fragment>
+  );
+};
 
 
 OrderDetail.propTypes = {
