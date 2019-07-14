2115fa1379d0d862bebdf65154c08e7b8cdbcb2e
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-07-14 03:59:09 +0100

    REMOVE: unused actions and reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,16 +2,10 @@ import { combineReducers } from 'redux';
 
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
-import customersReducer from './customersReducers';
-import categoryReducer from './categoryReducer';
-import menuReducer from './menuReducer';
 import ordersReducer from './ordersReducer';
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  customers: customersReducer,
-  categories: categoryReducer,
-  menus: menuReducer,
   orders: ordersReducer,
 });
