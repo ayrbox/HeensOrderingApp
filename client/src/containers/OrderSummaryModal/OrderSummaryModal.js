f689ffd458273fd86a9b8304b531360ef1e444ce
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-07-26 15:04:49 +0100

    Pick order parameters from redux store to process order

--- client/src/containers/OrderSummaryModal/OrderSummaryModal.js
+++ client/src/containers/OrderSummaryModal/OrderSummaryModal.js
@@ -4,6 +4,8 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import Button from '@material-ui/core/Button';
+import pick from 'lodash/pick';
+
 import OrderDetails from './components/OrderDetails';
 import ConfirmAction from '../../components/ConfirmAction';
 
@@ -14,6 +16,23 @@ const OrderSummaryModal = ({
   processOrder,
 }) => {
   const { openSummary } = order;
+
+  const handleProcesOrder = () => {
+    const orderToProcess = {
+      ...pick(order, [
+        'orderType',
+        'deliveryAddress',
+        'tableNo',
+        'orderItems',
+        'subTotal',
+        'discount',
+        'orderTotal',
+      ]),
+      date: new Date(),
+    };
+    processOrder(orderToProcess);
+  };
+
   return (
     <Dialog
       open={openSummary}
@@ -30,7 +49,7 @@ const OrderSummaryModal = ({
       </DialogContent>
       <DialogActions>
         <ConfirmAction
-          action={processOrder}
+          action={handleProcesOrder}
           message="Please click yes to confirm and process order?"
         >
           <Button
