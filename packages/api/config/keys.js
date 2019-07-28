c7883a71dd0754aadbfa636dbe3cde82ee807ecf
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-07-29 00:42:00 +0100

    make development default environment if not supplied

--- packages/api/config/keys.js
+++ packages/api/config/keys.js
@@ -2,12 +2,14 @@ const keysProduction = require('./keys_prod');
 const keysDevelopment = require('./keys_dev');
 const keysTest = require('./keys_test');
 
-if (process.env.NODE_ENV === 'production') {
+const { NODE_ENV = 'development' } = process.env;
+
+if (NODE_ENV === 'production') {
   module.exports = keysProduction;
-} else if (process.env.NODE_ENV === 'test') {
+} else if (NODE_ENV === 'test') {
   module.exports = keysTest;
 } else {
   module.exports = keysDevelopment;
 }
 
-console.log(`ENVIRONMENT: ${process.env.NODE_ENV}`); // eslint-disable-line
+console.log(`ENVIRONMENT: ${NODE_ENV}`); // eslint-disable-line
