fbaa2fd131caa6bcea6e8a8c994c8e81865a29f8
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-07-29 02:06:07 +0100

    add option to run dev api and api using docker

--- packages/api/config/keys_dev.js
+++ packages/api/config/keys_dev.js
@@ -1,4 +1,9 @@
+const {
+  MONGO_URI = 'mongodb://localhost:27017/db_heens',
+  SECRET_KEY = 'app-secret-key',
+} = process.env;
+
 module.exports = {
-  mongoURI: 'mongodb://localhost:27017/db_heens',
-  secretKey: 'app-secret-key',
+  mongoURI: MONGO_URI,
+  secretKey: SECRET_KEY,
 };
--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -23,7 +23,7 @@ app.use(routes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../', 'public', 'index.html'));
   });
 }
 
