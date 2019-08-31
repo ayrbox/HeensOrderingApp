4286d5caba810fe813878848daf9d9888b68ce2b
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-09-01 00:26:27 +0100

    Add test for discount selectlist

---
+++ packages/app/src/containers/OrderSummaryModal/components/Discount/Discount.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Discount from './Discount';
+import { DISCOUNT_PERCENTAGES } from '../../../../constants';
+
+describe('<Discount />', () => {
+  it('should render correctly', () => {
+    const mockOnChange = jest.fn();
+
+    const {
+      getByTestId,
+      getByRole,
+      getAllByTestId,
+      getByText,
+    } = render(
+      <Discount discount={10} onChange={mockOnChange} />,
+    );
+
+    getByTestId('discount-dropdown');
+    const discountSelect = getByRole('button');
+    fireEvent.click(discountSelect);
+
+    const discountItems = getAllByTestId('discount-item');
+    expect(discountItems).toHaveLength(DISCOUNT_PERCENTAGES.length);
+
+    const sampleDiscount = 20;
+    const discountRegEx = new RegExp(`${sampleDiscount}%`);
+
+    const discountItem = discountItems.find(item => item.textContent.match(discountRegEx));
+    fireEvent.click(discountItem);
+    expect(mockOnChange).toHaveBeenCalledWith(sampleDiscount);
+  });
+});
