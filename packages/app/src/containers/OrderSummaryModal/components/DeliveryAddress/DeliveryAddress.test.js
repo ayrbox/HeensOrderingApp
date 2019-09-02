fb3bc583464048c67af51fbad12cbcf0e0d47c6e
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-09-02 20:08:52 +0100

    WIP: Test deliveryaddress

---
+++ packages/app/src/containers/OrderSummaryModal/components/DeliveryAddress/DeliveryAddress.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import DeliveryAddress from './DeliveryAddress';
+
+describe('<DeliveryAdress />', () => {
+  it('should render correctly', () => {
+        
+  });
+});
